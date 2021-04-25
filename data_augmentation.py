076c625be2e8c5d4144bc0fdce817782b22c4e65
ssling0817 <70581390+ssling0817@users.noreply.github.com>
2021-04-25 14:04:20 +0800

    Add files via upload

--- data_augmentation.py
+++ data_augmentation.py
@@ -3,19 +3,20 @@
 
 # In[1]:
 
-
+import math
 import os
 import glob
 import cv2
 import random
 import numpy as np
+from tqdm import tqdm
 
 
 # In[2]:
 
 
-def brightness(img, low, high):
-    value = random.uniform(low, high)
+def brightness(img, value):
+    #value = random.uniform(low, high)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     hsv = np.array(hsv, dtype = np.float64)
     hsv[:,:,1] = hsv[:,:,1]*value
@@ -36,11 +37,113 @@ def vertical_flip(img, flag):
     else:
         return img
 def rotation(img, angle):
-    angle = int(random.uniform(-angle, angle))
+    #angle = int(random.uniform(-angle, angle))
     h, w = img.shape[:2]
     M = cv2.getRotationMatrix2D((int(w/2), int(h/2)), angle, 1)
     img = cv2.warpAffine(img, M, (w, h))
     return img
+
+def rotation_matrix(x, y, angle):
+    rad = math.radians(angle)
+    rx = x * math.cos(rad) - y * math.sin(rad)
+    ry = x * math.sin(rad) + y * math.cos(rad)
+    return rx, ry
+
+star = 'Sagittarius'
+source_Image_Folder = 'org/Image/' + star + '/'
+source_Label_Folder = 'org/Label/' + star + '/'
+target_Image_Folder = 'new/Image/' + star + '/'
+target_Label_Folder = 'new/Label/' + star + '/'
+
+### rotate * 3
+num_angle_aug = 3
+for imgpath in tqdm(glob.glob(source_Image_Folder + '*')):
+    name = imgpath.split('/', 4)[3].split('.')[0]
+    anglist = []
+    for i in range(num_angle_aug):
+        angle = 10
+        angle = int(random.uniform(-angle, angle))
+        while angle == 0 or angle in anglist:
+            angle = 10
+            angle = int(random.uniform(-angle, angle))
+        anglist.append(angle)
+    anglist.append(0)
+    for angle in anglist:
+        flag = True
+        path = source_Label_Folder + name + '.txt'
+        infile = open(path, 'r')
+        for line in infile:
+            data = line.split(' ')
+            x, y = float(data[1]) - 0.5, float(data[2]) - 0.5
+            x, y = rotation_matrix(x, y, angle * (-1))
+            x, y = x + 0.5, y + 0.5
+            if x > 1 or x < 0 or y > 1 or y < 0:
+                flag = False
+                break
+            outfile = open(target_Label_Folder + name + '_rotation' + str(angle) + '.txt', 'w')
+            data[1], data[2] = str(x)[:8], str(y)[:8]
+            outfile.write(' '.join(data))
+            outfile.close()
+        infile.close()
+        if flag:
+            img = cv2.imread(imgpath)
+            img_r = rotation(img, angle)
+            cv2.imwrite(target_Image_Folder + name + '_rotation' + str(angle) + '.jpg', img_r)
+
+### brightness * 4
+num_brightness_aug = 4
+for imgpath in tqdm(glob.glob(target_Image_Folder + '*')):
+    img = cv2.imread(imgpath)
+    name = imgpath.split('/', 4)[3].split('.')[0]
+    valist = []
+    for i in range(num_brightness_aug):
+        if i % 2 == 0:
+            low, high = 0.4, 0.8
+        else:
+            low, high = 1.2, 1.6
+        value = random.uniform(low, high)
+        valist.append(value)
+    for value in valist:
+        img_r = brightness(img, value)
+        cv2.imwrite(target_Image_Folder + name + '_brightness' + str(value)[:5] + '.jpg', img_r)
+        path = target_Label_Folder + name + '.txt'
+        infile = open(path, 'r')
+        for line in infile:
+            outfile = open(target_Label_Folder + name + '_brightness' + str(value)[:5] + '.txt', 'w')
+            outfile.write(line)
+            outfile.close()
+        infile.close()
+
+### horizontal_flip & vertical_flip
+for imgpath in tqdm(glob.glob(target_Image_Folder + '*')):
+    img = cv2.imread(imgpath)
+    name =  imgpath.split('/', 4)[3].split('.')[0]
+    img_r = horizontal_flip(img, True)
+    cv2.imwrite(target_Image_Folder + name + '_horizontal' + '.jpg', img_r)
+    img_r = vertical_flip(img, True)
+    cv2.imwrite(target_Image_Folder + name + '_vertical' + '.jpg', img_r)
+    path = target_Label_Folder + name + '.txt'
+    infile = open(path, 'r')
+    for line in infile:
+        data = line.split(' ')
+        x = float(data[1]) - 0.5
+        x = x * (-1)
+        x = x + 0.5
+        outfile = open(target_Label_Folder + name + '_horizontal' + '.txt', 'w')
+        data[1] = str(x)
+        outfile.write(' '.join(data))
+        outfile.close()
+        data = line.split(' ')
+        y = float(data[2]) - 0.5
+        y = y * (-1)
+        y = y + 0.5
+        outfile = open(target_Label_Folder + name + '_vertical' + '.txt', 'w')
+        data[2] = str(y)[:8]
+        outfile.write(' '.join(data))
+        outfile.close()
+    infile.close()
+
+
 #img = rotation(img, 30)    
 #img = vertical_flip(img, True)
 #img = horizontal_flip(img, True)
--- detect_uav.py
+++ detect_uav.py
@@ -31,11 +31,50 @@ import cv2
 
 import threading
 import socket
+import pyscreenshot as ImageGrab
+#import keyboard
+from pynput import keyboard
+from pynput.keyboard import Key, Listener 
+#00
 
-# cap=cv2.VideoCapture(0)
 cap=cv2.VideoCapture("udp://192.168.10.1:11111")	
 
+target="Gemini"
+num=1
+def save_result():
+    global num
+    global target
+    image = ImageGrab.grab()
+    
+    # 儲存檔案
+    image.save("result/"+target+str(num)+".jpg")
+    num=num+1
+tello_address = ('192.168.10.1', 8889)
+def land():
+    global tello_address
+    sock.sendto("land".encode(encoding="utf-8"),tello_address)		
+    time.sleep(1)
+    print("land")
 
+'''
+class result_thread(threading.Thread):
+    def __init__(self):	
+        threading.Thread.__init__(self)
+    def on_press(key): 
+        if(key==Key.f1):
+            save_result() 
+    with Listener(on_press=on_press) as listener: 
+        listener.start() 
+res_t=result_thread()
+res_t.start()
+'''
+def on_press(key): 
+    if(key==Key.f1):
+        save_result() 
+    if(key==Key.f5):
+        land() 
+listener = keyboard.Listener(on_press=on_press)
+listener.start() 
 #get frame in background
 current_frame=None 
 class cam_thread(threading.Thread):
@@ -55,12 +94,12 @@ cam_t.start()
 
 host = ''			
 recv_data=""			
-port = 9000			
+port = 9000		
 locaddr = (host,port)			
 			
 # Create a UDP socket			
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)			
-tello_address = ('192.168.10.1', 8889)			
+			
 sock.bind(locaddr)			
 			
 
@@ -76,9 +115,9 @@ class stop_thread(threading.Thread):
         while True:			
             print("send stop")			
             sock.sendto("stop".encode(encoding="utf-8"), tello_address)			
-            time.sleep(10)			
+            time.sleep(6)			
 stop_t=stop_thread()			
-			
+		
 def recv():						
     global recv_data			
     while True:			
@@ -90,17 +129,41 @@ def recv():
         except Exception:			
             print ('\nExit . . .\n')			
             break			
-
+#00
 #todo: connect to UAV and open video stream
 #(you can use lab1/example.py to send "streamon" to UAV first)
 
+
+
+print ('\r\n\r\nTello Python3 Demo.\r\n')
+
+print ('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
+
+print ('end -- quit demo.\r\n')
+
+
+
+
+
+msg = "command"
+msg = msg.encode(encoding="utf-8") 
+sent = sock.sendto(msg, tello_address)	
+time.sleep(2)
+print('command f')
+
+'''
+msg="streamon"
+msg = msg.encode(encoding="utf-8") 
+sent = sock.sendto(msg, tello_address)	
+time.sleep(0.1)
+'''
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
-    parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
+    parser.add_argument("--weights_path", type=str, default="weights/yolov3_ckpt_194.pth", help="path to weights file")
+    parser.add_argument("--class_path", type=str, default="data/custom.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.5, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
@@ -125,6 +188,7 @@ if __name__ == "__main__":
     else:
         # Load checkpoint weights
         model.load_state_dict(torch.load(opt.weights_path, map_location=torch.device('cpu')))
+        print("load custom.pth")
 
     model.eval()  
 
@@ -137,42 +201,73 @@ if __name__ == "__main__":
     fr = 0
     r = 1
 
-    target="aquarius"			
-    #cap=cv2.VideoCapture('pred2.mp4')			
-    sock.sendto("takeoff".encode(encoding="utf-8"), tello_address)		
-    time.sleep(3)			
-    stop_t.start()
+    
 
-    #cap=cv2.VideoCapture('udp://196.168.10.1:11111')
-    #cap=cv2.VideoCapture(0)
+    sock.sendto("takeoff".encode(encoding="utf-8"), tello_address)		
+    time.sleep(0.03)
+    print("takeoff")
+    time.sleep(3)
+    sock.sendto("forward 100".encode(encoding="utf-8"),tello_address)		
+    time.sleep(1)
+    print("forward 30")
+    
+    detect=False
+    finish=False
+    stop_t.start()
+#00
+
+    
+    f_flag = 0
+    unseen=0
+    width=960
+    height=720
+    ran=150
+    sleep_time=0.3
+    big=False
+    while(cap.isOpened()==False):
+        print("---------------opening cap--------------------")
+        cap.open("udp://192.168.10.1:11111")
+
+   # msg = msg.encode(encoding="utf-8") 
+    #sent = sock.sendto("up 80".encode(encoding="utf-8"), tello_address)
+    #print('up 80')	
+    #time.sleep(1)
+
     while (cap.isOpened()):
         time.sleep(0.05)
         #fr+=1
         #ret, frame = cap.read()
         frame=current_frame
-
+        
         if (fr % r == 0):
+                       
+
+	
             input_imgs, _ = transforms.Compose([DEFAULT_TRANSFORMS, Resize(opt.img_size)])((frame,np.zeros((1, 5))))
             input_imgs = Variable(input_imgs.type(Tensor)).unsqueeze(0)
-
+            #print ('---------------transforming---------------')
             # Get detections
             with torch.no_grad():
                 detections = model(input_imgs)
                 detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)
-
+            #print ('---------------Create plot---------------')
             # Create plot
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             plt.figure()
             fig, ax = plt.subplots(1)
             ax.imshow(img)
             
-            print ('-----------------------------------------')
+            #print ('---------------Detection-----------------')
 
             if detections is not None:
                 # Rescale boxes to original image
+#00
+                
                 detections = rescale_boxes(detections[0], opt.img_size, img.shape[:2])
 
                 for x1, y1, x2, y2, conf, cls_pred in detections:
+                    #if(classes[int(cls_pred)]!=target):
+                    #    continue;
                     box_w = x2 - x1
                     box_h = y2 - y1
 
@@ -204,10 +299,91 @@ if __name__ == "__main__":
 
 
                     #todo: send command to control UAV
-
+                    if(finish):
+                        print('finish')
+                        continue
 
 
                     #------
+                    if(classes[int(cls_pred)]!=target and big):
+                        unseen+=1
+                        if(unseen>20):
+                            sock.sendto("back 20".encode(encoding="utf-8"), tello_address)
+                            time.sleep(sleep_time)
+                            print("back 20")
+                            unseen=0
+                            big=False
+                       # f_flag-=1
+                    if(classes[int(cls_pred)]==target):
+                        detect=True
+                        if(box_w>960*0.7 or box_h>720*0.7):
+                            finish=True
+                            print('big enough!Capture!')
+                        if(box_w>960*0.2 or box_h>720*0.2):
+                            ran=60
+                        if((x_center<width/2+ran)and(x_center>width/2-ran)and(y_center>height/2-ran)and(y_center<height/2+ran)):
+                            if(box_w>960*0.3 or box_h>720*0.3):
+                                sock.sendto("forward 20".encode(encoding="utf-8"),tello_address)		
+                                time.sleep(sleep_time)
+                                print("forward 20")
+                                big=True
+                            else:
+                                sock.sendto("forward 40".encode(encoding="utf-8"),tello_address)		
+                                time.sleep(sleep_time)
+                                print("forward 40")
+                           
+                            '''
+                            if(f_flag<=2):
+                                sock.sendto("forward 40".encode(encoding="utf-8"),tello_address)		
+                                time.sleep(1)
+                                print("forward 40")
+                                ran=ran*2/3
+                                f_flag+=1
+                                print("forward numbers",f_flag) 
+                            '''
+
+                            
+
+                        else:
+                            if(x_center<width/2-ran):
+                  
+                                sock.sendto("left 20".encode(encoding="utf-8"), tello_address)		
+                                time.sleep(sleep_time)
+                                print("left 20")
+                            elif(x_center>width/2+ran):
+                                sock.sendto("right 20".encode(encoding="utf-8"), tello_address)		
+                                time.sleep(sleep_time)
+                                print("right 20")
+
+                            if(y_center<height/2-ran):
+                  
+                                sock.sendto("up 20".encode(encoding="utf-8"), tello_address)		
+                                time.sleep(sleep_time)	
+                                print("up 20")
+
+                            elif(y_center>height/2+ran):
+                                sock.sendto("down 20".encode(encoding="utf-8"), tello_address)		
+                                time.sleep(sleep_time)
+                                print("down 20")
+
+                        
+                    #else:
+                    #      if(detect==False):
+                    #          msg = "up 30"
+                    #          msg = msg.encode(encoding="utf-8") 
+                    #          sent = sock.sendto(msg, tello_address)
+                    #          print('up 30')
+                    if(detect==False):
+                        msg = "up 20"
+                        msg = msg.encode(encoding="utf-8") 
+                        sent = sock.sendto(msg, tello_address)
+                        print('up 20')
+                        time.sleep(2) 
+                        sock.sendto("forward  50".encode(encoding="utf-8"),tello_address)		
+                        time.sleep(1)
+                        print("forward 30")
+    
+
                     	
                 plt.axis("off")
                 plt.gca().xaxis.set_major_locator(NullLocator())
@@ -217,13 +393,20 @@ if __name__ == "__main__":
                 img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
                 img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
                 cv2.imshow("plot",img)
+            #else:
+            #    if(detect==False):
+            #        msg = "up 40"
+            #        msg = msg.encode(encoding="utf-8") 
+            #        sent = sock.sendto(msg, tello_address)
+            #        print('up 40')	
             plt.close('all')
-                
+
+   
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        
+#cv2.imwrite("result/"+target+str(num)+".jpg", img)
 
     cap.release()
     cv2.destroyAllWindows()
-
+time.sleep(3)
